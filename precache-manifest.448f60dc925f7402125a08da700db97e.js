7862500615bb3cc5aedeb972c249a6d5e8c876c2
David Rodriguez Fuentes <davidrf.developer@gmail.com>
2019-06-25 01:00:00 -0400

    Updates

---
+++ precache-manifest.448f60dc925f7402125a08da700db97e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6275d8d4721fdaf78bfabe460bd7f242",
+    "url": "/apprentice-a11y-web-workshop/index.html"
+  },
+  {
+    "revision": "523c51344efc635f6787",
+    "url": "/apprentice-a11y-web-workshop/static/css/main.5d79e513.chunk.css"
+  },
+  {
+    "revision": "caa9a7fa08bf2c0d73b8",
+    "url": "/apprentice-a11y-web-workshop/static/js/2.f0910273.chunk.js"
+  },
+  {
+    "revision": "523c51344efc635f6787",
+    "url": "/apprentice-a11y-web-workshop/static/js/main.5f91434a.chunk.js"
+  },
+  {
+    "revision": "e92124ff0eac5c754660",
+    "url": "/apprentice-a11y-web-workshop/static/js/runtime~main.c139e76d.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/apprentice-a11y-web-workshop/static/media/logo.5d5d9eef.svg"
+  }
+]);
